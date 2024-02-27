a25e1039ea263bb444a423af605ae68390187411
amel83 <tesfaneshk222@gmail.com>
2024-02-27 12:30:49 +0000

    jgu

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Logs a me to ole.
+ * @param {string} message - message to log.
+ * @returns {void}
+ */
+// Arrow fun
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the hvhjge fun
+ * @module displge
+ * @function jhuyb jhui
+ */
+// Default module.
+module.exports = displayMessage;
